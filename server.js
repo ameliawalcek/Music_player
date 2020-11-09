a6eeb78da7b19086a18c35cc9a6aed1e559831f8
amelia <ameliawalcek@gmail.com>
2020-11-09 15:08:42 +0200

    server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const app = express()
+
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors())
+
+
+
+// T7TIxsTsFBSkRtQR
+// miaTopia
