5c4ef17f0337cdcebfbffd2f0633c48e23450dc5
amelia <ameliawalcek@gmail.com>
2020-11-15 11:09:05 +0200

    starting controllers

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = require("express")
-const mongoose = require('mongoose')
-const bodyParser = require("body-parser")
-const cors = require("cors")
+import express from "express"
+import mongoose from 'mongoose'
+import bodyParser from "body-parser"
+import cors from "cors"
+// import postRoutes from './server/routes/posts.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
-app.use(bodyParser.json({limit: '30mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+// app.use('/posts', postRoutes)
+
+const { CONNECTION_URL, PORT } = process.env
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
+// const PORT = process.env.PORT || 5000
 
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
+    .catch((err) => console.log(err.message))
 
-// T7TIxsTsFBSkRtQR
-// miaTopia
+mongoose.set('useFindAndModify', false)
